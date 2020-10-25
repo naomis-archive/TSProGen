8b60e0ff74829f5bc50e1151d318ebdf5ddcac5b
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2020-10-25 09:15:52 -0700

    It generates the readme now! (#8)
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- src/bin.ts
+++ src/bin.ts
@@ -1,1 +1,14 @@
-console.log("The CLI works!");
+import fs from "fs";
+import path from "path";
+
+fs.readFile(path.join(__dirname, "./readme-copy.md"), "utf-8", (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  fs.writeFile(path.join(process.cwd() + "/README.md"), data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Readme generated");
+  });
+});
