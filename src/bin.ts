302c24535f873f01efaaba2f46b630133fb7f6ab
SkyWolf369 <Captainawesome829@gmail.com>
2020-10-25 13:55:18 -0400

    Added Package.json Gen

--- src/bin.ts
+++ src/bin.ts
@@ -12,3 +12,16 @@ fs.readFile(path.join(__dirname, "./readme-copy.md"), "utf-8", (err, data) => {
     console.log("Readme generated");
   });
 });
+
+fs.readFile(path.join(__dirname, "./package-copy.json"), "utf-8", (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  fs.writeFile(path.join(process.cwd() + "/test-package.json"), data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Package.json generated generated");
+  });
+});
+
