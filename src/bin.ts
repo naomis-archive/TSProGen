1add910b0b2d23bd08d327020d4fbf197ff60369
Theo Paris <theoparisdesigns@gmail.com>
2020-11-01 23:00:03 +0000

    added package manager choice

--- src/bin.ts
+++ src/bin.ts
@@ -1,28 +1,66 @@
-import fse from "fs-extra";
+import fs from "fs-extra";
+import inquirer from "inquirer";
+import os from "os";
+import { exec } from "child_process";
+import { promisify } from "util";
 
-/* function getFiles(dir: string): string[][] {
-  const files = [];
-  const dirents = fs.readdirSync(dir, { withFileTypes: true });
-  for (const dirent of dirents) {
-    const res = path.resolve(dir, dirent.name);
-    if (dirent.isDirectory()) {
-      return files.concat(getFiles(res));
-    } else {
-      files.push([res, dir]);
-    }
-  }
-  return files;
-} */
-try {
-  const templateDir = `${__dirname}/../src/template`;
-  const targetDir = process.cwd();
-  if (!fse.existsSync(targetDir)) {
-    fse.mkdirSync(targetDir, { recursive: true });
-  }
-  fse.copySync(templateDir, targetDir, {
-    errorOnExist: true,
-    overwrite: false,
-  });
-} catch (err) {
-  console.error(err);
+const execAsync = promisify(exec);
+
+export interface Config {
+  packageManager: string;
 }
+
+const createProject = async () => {
+  try {
+    const templateDir = `${__dirname}/../src/template`;
+    const targetDir = process.cwd();
+    if (!fs.pathExists(targetDir)) {
+      await fs.mkdir(targetDir);
+    }
+    await fs.copy(templateDir, targetDir, {
+      errorOnExist: true,
+      overwrite: false,
+    });
+    return true;
+  } catch (err) {
+    console.error(`Failed to copy template files: ${err.message}`);
+    return false;
+  }
+};
+
+const configFile = `${os.homedir()}/.config/tsprogen.json`;
+const getConfig = async (): Promise<Config> => {
+  if (!(await fs.pathExists(configFile))) {
+    const { pm } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "pm",
+        message: "What package manager would you like to use?",
+        default: "npm",
+        choices: ["pnpm", "npm", "yarn"],
+      },
+    ]);
+    await fs.writeFile(configFile, JSON.stringify({ packageManager: pm }), {
+      encoding: "utf-8",
+    });
+  }
+  return JSON.parse(await fs.readFile(configFile, { encoding: "utf-8" }));
+};
+
+getConfig()
+  .then(async ({ packageManager }) => {
+    const result = await createProject();
+    if (!result) {
+      return;
+    }
+    const { stdout, stderr } = await execAsync(`${packageManager} install`, {
+      cwd: process.cwd(),
+    });
+    if (stderr) {
+      console.error(`Install error: ${stderr}`);
+    }
+    console.log(`Install log: ${stdout}`);
+  })
+  .catch(() => {
+    console.error(`Could not read config file ${configFile}!`);
+  });
