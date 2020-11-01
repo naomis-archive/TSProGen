36eff1e2caae991539055ce91b845a1c231e51b5
Theo Paris <theoparisdesigns@gmail.com>
2020-11-01 23:19:01 +0000

    using listr2 in order to have a prompt and a nice task list

--- src/bin.ts
+++ src/bin.ts
@@ -1,8 +1,8 @@
 import fs from "fs-extra";
-import inquirer from "inquirer";
 import os from "os";
 import { exec } from "child_process";
 import { promisify } from "util";
+import { Listr, ListrRenderer, ListrTaskWrapper } from "listr2";
 
 const execAsync = promisify(exec);
 
@@ -10,7 +10,14 @@ export interface Config {
   packageManager: string;
 }
 
-const createProject = async () => {
+export interface TaskCtx {
+  config: Config;
+}
+
+const createProject = async (
+  _: TaskCtx,
+  task: ListrTaskWrapper<TaskCtx, typeof ListrRenderer>
+) => {
   try {
     const templateDir = `${__dirname}/../src/template`;
     const targetDir = process.cwd();
@@ -21,46 +28,61 @@ const createProject = async () => {
       errorOnExist: true,
       overwrite: false,
     });
-    return true;
   } catch (err) {
-    console.error(`Failed to copy template files: ${err.message}`);
-    return false;
+    task.output = `Failed to copy template files: ${err.message}`;
+    throw err;
   }
 };
 
 const configFile = `${os.homedir()}/.config/tsprogen.json`;
-const getConfig = async (): Promise<Config> => {
+const getConfig = async (
+  ctx: TaskCtx,
+  task: ListrTaskWrapper<TaskCtx, typeof ListrRenderer>
+): Promise<void> => {
   if (!(await fs.pathExists(configFile))) {
-    const { pm } = await inquirer.prompt([
+    ctx.config = await task.prompt([
       {
         type: "list",
-        name: "pm",
+        name: "packageManager",
         message: "What package manager would you like to use?",
         default: "npm",
         choices: ["pnpm", "npm", "yarn"],
       },
     ]);
-    await fs.writeFile(configFile, JSON.stringify({ packageManager: pm }), {
+    await fs.writeFile(configFile, JSON.stringify(ctx.config), {
       encoding: "utf-8",
     });
   }
-  return JSON.parse(await fs.readFile(configFile, { encoding: "utf-8" }));
+  ctx.config = JSON.parse(await fs.readFile(configFile, { encoding: "utf-8" }));
 };
 
-getConfig()
-  .then(async ({ packageManager }) => {
-    const result = await createProject();
-    if (!result) {
-      return;
-    }
-    const { stdout, stderr } = await execAsync(`${packageManager} install`, {
-      cwd: process.cwd(),
-    });
-    if (stderr) {
-      console.error(`Install error: ${stderr}`);
-    }
-    console.log(`Install log: ${stdout}`);
-  })
-  .catch(() => {
-    console.error(`Could not read config file ${configFile}!`);
+const installDependencies = async (
+  ctx: TaskCtx,
+  task: ListrTaskWrapper<TaskCtx, typeof ListrRenderer>
+) => {
+  const { stderr } = await execAsync(`${ctx.config.packageManager} install`, {
+    cwd: process.cwd(),
   });
+  if (stderr) {
+    task.output = `Install error: ${stderr}`;
+    throw new Error(stderr);
+  }
+};
+
+const tasks = new Listr<TaskCtx>([
+  {
+    title: "Retrieve Config",
+    task: getConfig,
+  },
+  {
+    title: "Create Project Files",
+    task: createProject,
+  },
+  {
+    title: "Install Dependencies",
+    task: installDependencies,
+  },
+]);
+
+// Run all project tasks
+tasks.run().catch(() => null);
