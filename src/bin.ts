e07403eee4967a3a6709d331c632416f422ddc9e
Theo Paris <theoplaysmc@gmail.com>
2020-11-01 17:08:58 -0800

    Package Manager Selection (#23)
    * using editorconfig instead of prettierrc
    
    * 1. made it cleaner by using template folder
    2. now using fs-extra for copying the files
    
    * added package manager choice
    
    * using listr2 in order to have a prompt and a nice task list
    
    * fixed template gitignore and error logging
    
    * fixed postbuild
    
    * added package-lock.json back in

--- src/bin.ts
+++ src/bin.ts
@@ -1,75 +1,111 @@
-import fs from "fs";
-import path from "path";
+import fs from "fs-extra";
+import os from "os";
+import { exec } from "child_process";
+import { promisify } from "util";
+import { Listr, ListrRenderer, ListrTaskWrapper } from "listr2";
 
-fs.readFile(path.join(__dirname, "./readme-copy.md"), "utf-8", (err, data) => {
-  if (err) {
-    return console.log(err);
-  }
-  fs.writeFile(path.join(process.cwd() + "/README.md"), data, (err) => {
-    if (err) {
-      return console.log(err);
+const execAsync = promisify(exec);
+
+export interface Config {
+  packageManager: string;
+}
+
+export interface TaskCtx {
+  config: Config;
+}
+
+const createProject = async () => {
+  try {
+    const templateDir = `${__dirname}/template`;
+    const targetDir = process.cwd();
+    if (!fs.pathExists(targetDir)) {
+      await fs.mkdir(targetDir);
     }
-    console.log("Readme generated");
+    await fs.copy(templateDir, targetDir, {
+      errorOnExist: true,
+      overwrite: false,
+    });
+  } catch (err) {
+    throw new Error(`Failed to copy template files: ${err.message}`);
+  }
+};
+
+const promptConfig = async (
+  ctx: TaskCtx,
+  task: ListrTaskWrapper<TaskCtx, typeof ListrRenderer>
+) => {
+  ctx.config = {
+    packageManager: await task.prompt([
+      {
+        type: "autocomplete",
+        name: "packageManager",
+        message: "What package manager would you like to use?",
+        default: "npm",
+        choices: ["pnpm", "npm", "yarn"],
+      },
+    ]),
+  };
+  await fs.writeFile(configFile, JSON.stringify(ctx.config), {
+    encoding: "utf-8",
   });
+};
+
+const configFile = `${os.homedir()}/.config/tsprogen.json`;
+const getConfig = async (
+  ctx: TaskCtx,
+  task: ListrTaskWrapper<TaskCtx, typeof ListrRenderer>
+): Promise<void> => {
+  if (!(await fs.pathExists(configFile))) {
+    await promptConfig(ctx, task);
+  }
+  ctx.config = JSON.parse(await fs.readFile(configFile, { encoding: "utf-8" }));
+
+  if (
+    !ctx.config.packageManager ||
+    (ctx.config.packageManager &&
+      !["npm", "pnpm", "yarn"].includes(ctx.config.packageManager.trim()))
+  ) {
+    await promptConfig(ctx, task);
+  }
+};
+
+const installDependencies = async (
+  ctx: TaskCtx,
+  task: ListrTaskWrapper<TaskCtx, typeof ListrRenderer>
+) => {
+  if (!["npm", "pnpm", "yarn"].includes(ctx.config.packageManager.trim())) {
+    throw new Error(`Install error: Invalid package manager in ${configFile}`);
+  }
+  const { stderr } = await execAsync(
+    `${ctx.config.packageManager.trim()} install`,
+    {
+      cwd: process.cwd(),
+    }
+  );
+  if (stderr) {
+    throw new Error(`Install error: ${stderr}`);
+  }
+};
+
+const tasks = new Listr<TaskCtx>(
+  [
+    {
+      title: "Retrieve Config",
+      task: getConfig,
+    },
+    {
+      title: "Create Project Files",
+      task: createProject,
+    },
+    {
+      title: "Install Dependencies",
+      task: installDependencies,
+    },
+  ],
+  { concurrent: false }
+);
+
+// Run all project tasks
+tasks.run().catch(() => {
+  // do nothing
 });
-
-fs.readFile(
-  path.join(__dirname, "./package-copy.json"),
-  "utf-8",
-  (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-    fs.writeFile(path.join(process.cwd() + "/package.json"), data, (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("Package.json generated");
-    });
-  }
-);
-
-fs.mkdir(path.join(process.cwd() + "/src"), (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log("src directory generated");
-});
-
-fs.writeFile(
-  path.join(process.cwd() + "/src/index.ts"),
-  "console.log('index.ts works!');",
-  (err) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("index.ts generated");
-  }
-);
-
-fs.readFile(
-  path.join(__dirname + "/../tsconfig.json"),
-  "utf-8",
-  (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
-    fs.writeFile(path.join(process.cwd() + "/tsconfig.json"), data, (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("tsconfig.json generated");
-    });
-  }
-);
-
-fs.writeFile(
-  path.join(process.cwd() + "/.gitignore"),
-  "/node_modules/",
-  (err) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(".gitignore generated");
-  }
-);
